ce364d8c4ded6784f6c8780c580bf56d6cf48589
maulanajulham <julhammaulana@gmail.com>
2025-07-20 10:51:57 +0700

    menambah halaman about dan profile

---
+++ app/about/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return <main>
+        <h1>Ini halaman About</h1>
+    </main>
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return <main>
+    <h1>Ini Halaman Contact</h1>
+    </main>
+}
